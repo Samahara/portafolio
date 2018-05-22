f24980f7baacfcdccd7dd5ad6325620ca4efcc20
Samahara <le.chat.au.chapeau@hotmail.com>
2018-05-22 18:23:22 -0500

    implementando nuevas vistas

--- js/app.js
+++ js/app.js
@@ -11,3 +11,33 @@ function gomainmenu() {
   $('#logohome').hide("fast");
   $('#mainmenu').show("fast");
 }
+
+$('.contacto').click(gotocontact)
+
+function gotocontact(e) {
+  var option = event.target;
+  console.log(option);
+  $('#portafolio').hide(300);
+  $('#sobre_mi').hide(300);
+  $('#contacto').show("slow");
+}
+
+$('.portafolio').click(gotoportafolio)
+
+function gotoportafolio(e) {
+  var option = event.target;
+  console.log(option);
+  $('#sobre_mi').hide(300);
+  $('#contacto').hide(300);
+  $('#portafolio').show("slow")
+}
+
+$('.sobre_mi').click(gotosobremi)
+
+function gotosobremi(e) {
+  var option = event.target;
+  console.log(option);
+  $('#portafolio').hide(300);
+  $('#contacto').hide(300);
+  $('#sobre_mi').show("slow");
+}
